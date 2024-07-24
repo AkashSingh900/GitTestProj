77070cf9e434f974c619ec1fb8147c4a8111b062
@Akash.Singh02 <akash.singh@riya.travel>
2024-07-24 17:41:40 +0530

    Add project files.

---
+++ GitTestProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
