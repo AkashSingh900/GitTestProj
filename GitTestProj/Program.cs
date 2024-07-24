7ba71d5406815464e0b62fb454dbe09a8951cd83
@Akash.Singh02 <akash.singh@riya.travel>
2024-07-24 17:43:26 +0530

    first  commit

--- GitTestProj/Program.cs
+++ GitTestProj/Program.cs
@@ -7,6 +7,7 @@ namespace GitTestProj
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Akash singh");
         }
     }
 }
